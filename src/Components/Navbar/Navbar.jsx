6f071dfaae5427b12cc74f2e9cb9c75493f491bc
Likith Mukkamala <likith@Likiths-MacBook-Pro.local>
2024-07-12 13:46:19 +0700

    Front-end Completed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,39 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../Context/ShopContext";
+import nav_dropdown_icon from "../Assets/nav_dropdown.png";
+
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
   const { getTotalCartItems } = useContext(ShopContext);
+  const menuRef = useRef();
+
+  const dropdown_toggle = (e) => {
+    {
+      /* This is to change the className to open whenever clicked on the image and removes it when clicked again*/
+    }
+    menuRef.current.classList.toggle("nav-menu-visible");
+    e.target.classList.toggle("open");
+  };
+  {
+    /*useRef hook is used to store reference to a DOM Element */
+  }
   return (
     <div className="navbar">
       <div className="nav-logo">
         <img src={logo} alt="" />
         <p>SHOPPER</p>
       </div>
-      <ul className="nav-menu">
+      <img
+        onClick={dropdown_toggle}
+        className="nav-dropdown"
+        src={nav_dropdown_icon}
+        alt=""
+      />
+      <ul ref={menuRef} className="nav-menu">
         <li
           onClick={() => {
             setMenu("shop");
